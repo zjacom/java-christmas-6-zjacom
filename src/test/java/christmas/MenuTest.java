61d1a9523479e7472b509ca4de4e5405b0d554fc
zjacom <eephus625@icloud.com>
2023-11-10 00:22:37 +0900

    test(christmas): 메뉴 이름으로 매치되는 가격을 가져오는 테스트 작성

---
+++ src/test/java/christmas/MenuTest.java
@@ -0,0 +1,17 @@
+package christmas;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class MenuTest {
+    @Test
+    void menuItemPriceTest() {
+        // given
+        Menu menu = new Menu();
+        // when
+        int menuPrice = menu.findMenuPrice("양송이스프");
+        // then
+        assertThat(menuPrice).isEqualTo(6000);
+    }
+}
