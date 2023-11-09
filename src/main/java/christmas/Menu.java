632e685498a660b9e51640b6cafaf2fdd0552867
zjacom <eephus625@icloud.com>
2023-11-10 00:23:58 +0900

    feat(christmas): 메뉴 이름으로 매치되는 가격을 가져오는 기능 추가

--- src/main/java/christmas/Menu.java
+++ src/main/java/christmas/Menu.java
@@ -1,13 +1,29 @@
 package christmas;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Menu {
-    // Menu에 있는 정보를 필드로 가진다.
+    // 필드(menu)에 메뉴를 넣는 더 좋은 방법을 찾아보자.
+    private Map<String, Integer> menu = new HashMap<>();
+    public Menu() {
+        menu.put("양송이스프", 6000);
+        menu.put("타파스", 5500);
+        menu.put("시저샐러드", 8000);
+        menu.put("티본스테이크", 55000);
+        menu.put("바비큐립", 54000);
+        menu.put("해산물파스타", 35000);
+        menu.put("크리스마스파스타", 25000);
+        menu.put("초코케이크", 15000);
+        menu.put("아이스크림", 5000);
+        menu.put("제로콜라", 300);
+        menu.put("레드와인", 60000);
+        menu.put("샴페인", 25000);
+    }
 
-    // (음식이름, 총개수)를 입력받아서 (음식이름, 가격)을 리턴
-    public Map<String, Integer> 주문내역_금액과_매칭(String 주문내역) {
-        return null;
+    // 음식 이름을 받으면 매치되는 가격 리턴
+    public int findMenuPrice(String menuName) {
+        return menu.get(menuName);
     }
 
     // 증정품 품목을 받아서 가격 정보를 리턴
