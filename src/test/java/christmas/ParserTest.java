8a94f49c5bf60154be3c1e254bf9025ca3f706a7
zjacom <eephus625@icloud.com>
2023-11-09 22:03:04 +0900

    test(christmas): 고객이 입력한 주문 형식을 문자열에서 Map으로 바꿨을 때 기대한 값이 나오는지 확인하는 테스트 작성

---
+++ src/test/java/christmas/ParserTest.java
@@ -0,0 +1,27 @@
+package christmas;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ParserTest {
+    @DisplayName("고객이 입력한 주문 형식을 문자열에서 Map으로 바꿨을 때 기대한 값이 나오는지 확인하는 테스트")
+    @Test
+    void inputValueParseToExpectedMap() {
+        // given
+        Parser parser = new Parser();
+        String inputValue = "타파스-1,제로콜라-1";
+
+        Map<String, Integer> expectedParsedValue = new HashMap<>();
+        expectedParsedValue.put("타파스", 1);
+        expectedParsedValue.put("제로콜라", 1);
+        // when
+        Map<String, Integer> parsedValue = parser.inputStrParseToMap(inputValue);
+        // then
+        boolean result = parsedValue.equals(expectedParsedValue);
+        assertThat(result).isTrue();
+    }
+}
