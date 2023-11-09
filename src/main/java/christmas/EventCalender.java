3557327b17ee0a850c8ce6a44951b856559dce68
zjacom <eephus625@icloud.com>
2023-11-10 04:06:31 +0900

    feat(christmas): 특별 할인 금액을 계산하는 기능 추가

--- src/main/java/christmas/EventCalender.java
+++ src/main/java/christmas/EventCalender.java
@@ -25,7 +25,7 @@ public class EventCalender {
         int weekdayDiscountAmount = 0;
         Calendar calendar = Calendar.getInstance();
         calendar.set(year, month, day);
-        if (calendar.get(Calendar.DAY_OF_WEEK) <= 5) {
+        if (calendar.get(Calendar.DAY_OF_WEEK) <= Calendar.THURSDAY) {
             for (String menuName : orderServer.getOrderCheck().keySet()) {
                 // 문자열을 비교할 때 equals와 == 차이점 공부
                 if (menuName.equals("아이스크림") || menuName.equals("초코케이크")) {
@@ -41,7 +41,7 @@ public class EventCalender {
         int weekendDiscountAmount = 0;
         Calendar calendar = Calendar.getInstance();
         calendar.set(year, month, day);
-        if (calendar.get(Calendar.DAY_OF_WEEK) >= 6) {
+        if (calendar.get(Calendar.DAY_OF_WEEK) >= Calendar.FRIDAY) {
             for (String menuName : orderServer.getOrderCheck().keySet()) {
                 // 비교할 조건이 너무 많다. -> 간단하게 표현할 수 있는 방법을 찾아보자.
                 if (menuName.equals("티본스테이크") || menuName.equals("바비큐립") || menuName.equals("해산물파스타") || menuName.equals("크리스마스파스타")) {
@@ -51,5 +51,14 @@ public class EventCalender {
             }
         }
         return weekendDiscountAmount;
+    }
+
+    public int getSpecialDiscountAmount(int day) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, day);
+        if (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY || day == 25) {
+            return 1000;
+        }
+        return 0;
     }
 }
