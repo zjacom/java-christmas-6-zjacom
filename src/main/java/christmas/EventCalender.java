c77471121621b9969c47bb0e68936270ad7ac828
zjacom <eephus625@icloud.com>
2023-11-10 02:15:30 +0900

    feat(christmas): 평일 할인 금액을 계산하는 기능 추가

--- src/main/java/christmas/EventCalender.java
+++ src/main/java/christmas/EventCalender.java
@@ -26,4 +26,19 @@ public class EventCalender {
         }
         return discountAmount;
     }
+
+    public int getWeekdayDiscountAmount(int day, OrderServer orderServer) {
+        int weekdayDiscountAmount = 0;
+        calendar.set(year, month, day);
+        if (calendar.get(Calendar.DAY_OF_WEEK) >= 1 && calendar.get(Calendar.DAY_OF_WEEK) <= 5) {
+            for (String menuName : orderServer.getOrderCheck().keySet()) {
+                // 문자열을 비교할 때 equals와 == 차이점 공부
+                if (menuName.equals("아이스크림") || menuName.equals("초코케이크")) {
+                    Integer menuQuantity = orderServer.getOrderCheck().get(menuName);
+                    weekdayDiscountAmount += 2023 * menuQuantity;
+                }
+            }
+        }
+        return weekdayDiscountAmount;
+    }
 }
