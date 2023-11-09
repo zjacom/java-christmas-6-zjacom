ccdb9d10a499acc928a4f0afec75ebfbb4ac002e
zjacom <eephus625@icloud.com>
2023-11-10 05:36:30 +0900

    test(christmas): 총 혜택 금액을 계산하는 테스트 작성

--- src/test/java/christmas/PosMachineTest.java
+++ src/test/java/christmas/PosMachineTest.java
@@ -2,6 +2,7 @@ package christmas;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
 
@@ -10,11 +11,23 @@ public class PosMachineTest {
     void checkExpectedTotalOrderAmount() {
         // given
         PosMachine posMachine = new PosMachine();
-        Parser parser = new Parser();
-        Map<String, Integer> orderInfo = parser.inputStrParseToMap("티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1");
+        OrderServer orderServer = new OrderServer();
+        orderServer.inputStrParseToMap("티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1");
+        Map<String, Integer> orderInfo = orderServer.getOrderCheck();
         // when
         int totalOrderAmount = posMachine.calculateTotalOrderAmount(orderInfo);
         // then
         assertThat(totalOrderAmount).isEqualTo(142000);
+    }
+
+    @Test
+    void checkExpectedTotalBenefitAmount() {
+        // given
+        PosMachine posMachine = new PosMachine();
+        // when
+        int totalBenefitAmount = posMachine.총혜택금액_계산(List.of(1000, 2000, 3000), 25000);
+        // then
+        assertThat(totalBenefitAmount).isEqualTo(31000);
+
     }
 }
