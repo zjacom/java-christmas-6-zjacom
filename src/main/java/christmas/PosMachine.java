b084cd5ecbb7ea5227b383f577420d402b88ecec
zjacom <eephus625@icloud.com>
2023-11-10 05:48:47 +0900

    feat(christmas): 예상 결제 금액을 계산하는 기능 추가

--- src/main/java/christmas/PosMachine.java
+++ src/main/java/christmas/PosMachine.java
@@ -19,25 +19,25 @@ public class PosMachine {
     }
 
     // 할인 정보와 증정품 가격을 입력받아 총 혜택 금액 계산
-    public int 총혜택금액_계산(List<Integer> discountInfo, int giftPrice) {
-        int totalDiscountAmount = getTotalDiscountAmount(discountInfo);
+    public int calculateTotalBenefitAmount(List<Integer> discountInfo, int giftPrice) {
+        int totalDiscountAmount = calculateTotalDiscountAmount(discountInfo);
         return totalDiscountAmount + giftPrice;
     }
 
     // 할인 정보를 입력받아 총 할인 금액을 계산
-    private int getTotalDiscountAmount(List<Integer> discountInfo) {
+    private int calculateTotalDiscountAmount(List<Integer> discountInfo) {
         int totalDiscountAmount = 0;
         if (!discountInfo.isEmpty()) {
-            for (int number : discountInfo) {
-                totalDiscountAmount += number;
+            for (int discountAmount : discountInfo) {
+                totalDiscountAmount += discountAmount;
             }
         }
         return totalDiscountAmount;
     }
 
     // 총주문 금액과 할인 정보를 입력받아서 예상결제금액 리턴
-    public int 예상결제금액_계산(List<Integer> 할인정보) {
-        return 0;
+    public int calculatePayment(List<Integer> discountInfo) {
+        return totalOrderAmount -= calculateTotalDiscountAmount(discountInfo);
     }
 
     public int getTotalOrderAmount() {
