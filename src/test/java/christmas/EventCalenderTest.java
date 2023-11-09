6d0830aa0adf2a01677aafc596c6ba4016328aa2
zjacom <eephus625@icloud.com>
2023-11-10 02:15:00 +0900

    test(christmas): 평일 할인 정보 가져오는 테스트 작성

--- src/test/java/christmas/EventCalenderTest.java
+++ src/test/java/christmas/EventCalenderTest.java
@@ -2,6 +2,7 @@ package christmas;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,5 +18,18 @@ public class EventCalenderTest {
         int dDayDiscountAmount = eventCalender.getDdayDiscountAmount(day);
         // then
         assertThat(dDayDiscountAmount).isEqualTo(1200);
+    }
+
+    @Test
+    void checkWeekdayDiscountAmount() {
+        // given
+        EventCalender eventCalender = new EventCalender();
+        OrderServer orderServer = new OrderServer();
+        // when
+        int day = 3;
+        orderServer.inputStrParseToMap("티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1");
+        int weekdayDiscountAmount = eventCalender.getWeekdayDiscountAmount(day, orderServer);
+        // then
+        assertThat(weekdayDiscountAmount).isEqualTo(4046);
     }
 }
