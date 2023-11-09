0bb47e0a61f49391d5eb05be35aa732b4620a9a0
zjacom <eephus625@icloud.com>
2023-11-09 22:04:37 +0900

    feat(christmas): 고객이 입력한 주문 형식을 문자열에서 Map으로 바꾸는 기능 추가

--- src/main/java/christmas/Parser.java
+++ src/main/java/christmas/Parser.java
@@ -1,10 +1,27 @@
 package christmas;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Parser {
-    // (주문 음식 : 갯수) 형태로 반환수
-    public Map<String, Integer> 입력받은_주문내용_Map으로_변경(String inputValue) {
-        return null;
+    
+    // (주문 음식 : 갯수) 형태로 반환
+    // 구분에 String 배열을 사용하는데, 이 방법이 제일 빠른 방법인지 확인 필요.
+    public Map<String, Integer> inputStrParseToMap(String inputValue) {
+        Map<String, Integer> parsedValue = new HashMap<>();
+
+        String[] splitByComma = inputValue.split(",");
+
+        for (String pair : splitByComma) {
+            String[] splitByHyphen = pair.split("-");
+
+            if (splitByHyphen.length == 2) {
+                String menuName = splitByHyphen[0];
+                int menuQuantity = Integer.parseInt(splitByHyphen[1]);
+
+                parsedValue.put(menuName, menuQuantity);
+            }
+        }
+        return parsedValue;
     }
 }
