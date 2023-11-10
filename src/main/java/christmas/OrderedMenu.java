bf67be2873879388c95e65b93b0a83d7538cd800
zjacom <eephus625@icloud.com>
2023-11-10 17:28:47 +0900

    feat(christmas): OrderedMenu에서 검증되고 파싱된 주문 내역 받아서 저장할 수 있게 수정

--- src/main/java/christmas/OrderedMenu.java
+++ src/main/java/christmas/OrderedMenu.java
@@ -1,30 +1,14 @@
 package christmas;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class OrderedMenu {
     // (주문 음식 : 갯수)를 저장하는 필드
-    Map<String, Integer> orderedMenu = new HashMap<>();
-    
-    // (주문 음식 : 갯수) 형태로 반환
-    // 구분에 String 배열을 사용하는데, 이 방법이 제일 빠른 방법인지 확인 필요.
-    public void inputMenuChangeToOrderedMenu(String inputValue) {
-        Map<String, Integer> parsedValue = new HashMap<>();
+    private final Map<String, Integer> orderedMenu;
 
-        String[] splitByComma = inputValue.split(",");
-
-        for (String pair : splitByComma) {
-            String[] splitByHyphen = pair.split("-");
-
-            if (splitByHyphen.length == 2) {
-                String menuName = splitByHyphen[0];
-                int menuQuantity = Integer.parseInt(splitByHyphen[1]);
-
-                parsedValue.put(menuName, menuQuantity);
-            }
-        }
-        this.orderedMenu = parsedValue;
+    // (주문 음식 : 갯수)를 입력받음
+    public OrderedMenu(Map<String, Integer> orderedMenu) {
+        this.orderedMenu = orderedMenu;
     }
 
     public Map<String, Integer> getOrderedMenu() {
