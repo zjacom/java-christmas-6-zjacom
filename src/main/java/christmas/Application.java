310d9810050fcfd1a64b1a025e2a61dee1d0f6df
zjacom <eephus625@icloud.com>
2023-11-10 17:29:18 +0900

    feat(christmas): 프로그램 로직 작성

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,48 @@
 package christmas;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Validation validation = new Validation();
+        // 날짜 입력
+        InputView inputView = new InputView();
+        int day = 0;
+        try {
+            day = validation.validateDay(inputView.readDate());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        // 메뉴 입력
+        Map<String, Integer> orderMenu = new HashMap<>();
+        try {
+            orderMenu = validation.validateOrder(inputView.readOrder());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        OrderedMenu orderedMenu = new OrderedMenu(orderMenu);
+        Map<String, Integer> orderInfo = orderedMenu.getOrderedMenu();
+        OutputView outputView = new OutputView();
+        outputView.printMenu(orderInfo);
+        PosMachine posMachine = new PosMachine();
+        Menu menu = new Menu();
+        posMachine.calculateTotalOrderAmount(orderedMenu, menu);
+        outputView.printOrderAmount(posMachine.getTotalOrderAmount());
+        EventManager eventManager = new EventManager();
+        eventManager.collectDiscountInfo(day, orderedMenu, posMachine);
+        List<Integer> discountInfo = eventManager.getDiscountInfo();
+        int giftPrice = eventManager.getGiftInfo(posMachine, menu);
+        outputView.printGiftMenu(eventManager.queryReceiveGift());
+        EventCalender eventCalender = new EventCalender();
+        outputView.printDiscountInfo(discountInfo, giftPrice, eventCalender.confirmWeekdayOrWeekend(day));
+        int totalBenefitAmount = posMachine.calculateTotalBenefitAmount(discountInfo, giftPrice);
+        outputView.printTotalBenefitInfo(totalBenefitAmount);
+        int payment = posMachine.calculatePayment(discountInfo);
+        outputView.printPayment(payment);
+        String badge = eventManager.getBadge(totalBenefitAmount);
+        outputView.printBadge(badge);
     }
 }
