bc8079f161bbb1360a45dd14f9c18f6262274681
zjacom <eephus625@icloud.com>
2023-11-13 17:20:40 +0900

    feat(christmas): 비즈니스 로직을 담당하는 서비스 클래스 생성

---
+++ src/main/java/model/Service.java
@@ -0,0 +1,53 @@
+package model;
+
+import java.util.List;
+import java.util.Map;
+
+public class Service {
+    PosMachine posMachine = new PosMachine();
+    Menu menu = new Menu();
+    EventManager eventManager = new EventManager();
+    private final Day day;
+    private final OrderedMenu orderedMenu;
+
+    public Service(String day, String orderedMenu) {
+        this.day = new Day(day);
+        this.orderedMenu = new OrderedMenu(orderedMenu);
+    }
+
+    public Map<String, Integer> getOrderedMenus() {
+        return orderedMenu.getOrderedMenu();
+    }
+
+    public int getTotalOrderPrice() {
+        return posMachine.getTotalOrderPrice();
+    }
+
+    public String getGiftName() {
+        return eventManager.checkCustomerCanGetGift(posMachine.getTotalOrderPrice());
+    }
+
+    public List<Integer> getDiscountDetails() {
+        return eventManager.getDiscountDetails();
+    }
+
+    public int getGiftPrice() {
+        return eventManager.getGiftPrice(menu);
+    }
+
+    public int getTotalBenefitPrice(int giftPrice) {
+        return posMachine.calculateTotalBenefitPrice(eventManager.getDiscountDetails(), giftPrice);
+    }
+
+    public int getPayment() {
+        return posMachine.calculatePayment(eventManager.getDiscountDetails());
+    }
+
+    public String getBadge(int totalBenefitAmount) {
+        return eventManager.getBadge(totalBenefitAmount);
+    }
+
+    public int getReservedDay() {
+        return day.getDay();
+    }
+}
