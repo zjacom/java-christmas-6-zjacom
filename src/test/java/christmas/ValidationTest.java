5592c04484d0a80e39915434adb5c8da3ee35c95
zjacom <eephus625@icloud.com>
2023-11-15 04:31:54 +0900

    test(christmas): 예외 테스트 추가

--- src/test/java/christmas/ValidationTest.java
+++ src/test/java/christmas/ValidationTest.java
@@ -67,6 +67,31 @@ public class ValidationTest {
     }
 
     @Test
+    void customerInputOrderMenuOnlyDrink() {
+        // given
+        Validation validation = new Validation();
+        // when
+        String inputDate = "레드와인-1";
+        // then
+        assertThatThrownBy(() -> validation.validateOrder(inputDate))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 음료만 주문 시, 주문할 수 없습니다.");
+    }
+
+    @Test
+    void customerInputOverTotalQuantity() {
+        // given
+        Validation validation = new Validation();
+        // when
+        String inputDate = "초코케이크-21";
+        // then
+        assertThatThrownBy(() -> validation.validateOrder(inputDate))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다.\n"
+                        + "(e.g. 시저샐러드-1, 티본스테이크-1, 크리스마스파스타-1, 제로콜라-3, 아이스크림-1의 총개수는 7개)");
+    }
+
+    @Test
     void inputDateOnlyDigit() {
         // given
         Validation validation = new Validation();
@@ -77,7 +102,6 @@ public class ValidationTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
     }
-
     @Test
     void inputDateHasNumberRange() {
         // given
